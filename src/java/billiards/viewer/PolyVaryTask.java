eae6b638eb38fcf26b64344e96f8e79eb8482133
Austin Lu <austinlu1235@gmail.com>
2024-08-01 11:11:23 -0600

    Colorcheck before draw is now unnecessary

--- src/java/billiards/viewer/PolyVaryTask.java
+++ src/java/billiards/viewer/PolyVaryTask.java
@@ -105,7 +105,7 @@ public final class PolyVaryTask extends Task<ObservableList<Storage>> {
             // The BoyanCodes method vary3() called by autoVary() can throw exceptions. We need to catch them
             // By taking a second to check the pixel color, we can potentially avoid all other work for this coord.
             int color = pixelColor(coord);
-            if(color != 0) break; 
+            if(color != 0) continue; 
             try {
                 localCodes = autoCodesFiltered(coord, shotExecutor);
             } catch(RuntimeException e) {
@@ -135,6 +135,7 @@ public final class PolyVaryTask extends Task<ObservableList<Storage>> {
                 futures.add(storageExecutor.submit(new PriorityCallable<Either<String, Storage>>() {
                         @Override
                         public Either<String, Storage> call() {
+                            /* I don't think we need to check anything here anymore, since the combination of the earlier color check and the vary check ensure this pixel stays empty
                             int color = pixelColor(coord);
                             if(color == -1) return Either.left("");
                             if(color != 0) {
@@ -142,6 +143,7 @@ public final class PolyVaryTask extends Task<ObservableList<Storage>> {
                                 PolyVaryTask.this.updateProgress(progress.incrementAndGet(), todo); // updateProgress is thread safe
                                 return Either.left("");
                             }
+                            */
                             Either<String, Storage> result = loadStorage(classCodeSeq);
                             if(!PolyVaryTask.this.isCancelled()) PolyVaryTask.this.updateProgress(progress.incrementAndGet(), todo); // updateProgress is thread safe
                             return result;
