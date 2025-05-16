0e23405847b26913251f77280cb3a1a70494f7a4
Zhaoyu Li <wot.dragon0407@gmail.com>
2025-05-16 10:45:31 -0600

    new layout and logic that handles and returns a list of coordinates

--- src/java/billiards/viewer/Tetrahedron.java
+++ src/java/billiards/viewer/Tetrahedron.java
@@ -3,8 +3,11 @@ package billiards.viewer;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javaslang.Tuple2;
 
@@ -17,67 +20,82 @@ import java.util.List;
 public final class Tetrahedron {
     // WARNING: Global mutable state
     // ------------------------------------------------------------
-    private static String xDefault = "";
-    private static String yDefault = "";
+    private static String coordsDefault = "";
     private static String epsDefault = "0.00000001";
     // ------------------------------------------------------------
 
-    private final TextField xTextField = new TextField();
-    private final TextField yTextField = new TextField();
+    private final TextArea coordsTextArea = new TextArea();
     private final TextField epsTextField = new TextField();
 
     private final Stage stage = new Stage();
 
-    private final List<Tuple2<Double, Double>> points = new ArrayList<>(3);
+    private final List<Tuple2<Double, Double>> originalPoints = new ArrayList<>();
+    private final List<Tuple2<Double, Double>> points = new ArrayList<>();
 
     public Tetrahedron() {
-        xTextField.setPromptText("X coordinate");
-        yTextField.setPromptText("Y coordinate");
+        if (coordsDefault.isEmpty()) coordsDefault = Utils.readFromFile("tetrahedron.txt");
+
         epsTextField.setPromptText("epsilon");
 
-        xTextField.setText(xDefault);
-        yTextField.setText(yDefault);
+        coordsTextArea.setPromptText("Coordinates");
+        coordsTextArea.setText(coordsDefault);
+        coordsTextArea.setPrefHeight(300);
         epsTextField.setText(epsDefault);
 
-        final HBox root = new HBox();
+        final VBox root = new VBox(10);
         final Scene scene = new Scene(root);
         final Button loadButton = new Button();
+        final HBox hbox = new HBox(10, epsTextField, loadButton);
 
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> stage.close());
 
+        Label label = new Label("Enter coordinates on each line. The x and y coordinates should be separated by a whitespace.");
         root.getChildren().addAll(
-                xTextField, yTextField, epsTextField,loadButton);
+                label, coordsTextArea, hbox);
         root.setSpacing(10);
         root.setPadding(new Insets(10));
 
         loadButton.setText("Calculate");
         loadButton.setOnAction(event -> {
-            xDefault = xTextField.getText();
-            yDefault = yTextField.getText();
+            coordsDefault = coordsTextArea.getText();
+
             epsDefault = epsTextField.getText();
 
-            final double x = Double.parseDouble(xTextField.getText());
-            final double y = Double.parseDouble(yTextField.getText());
+            final String[] coords = coordsTextArea.getText().split("\n");
             final double eps = Double.parseDouble(epsTextField.getText());
 
-            final double x1_out = x - (eps * Math.sqrt(3)/2);
-            final double y1_out = y + (eps / 2);
-            final double x2_out = x + (eps * Math.sqrt(3)/2);
-            final double y2_out = y + (eps / 2);
-            final double y3_out = y - eps;
+            for (String coord : coords) {
+                if (coord.startsWith("//") || coord.trim().isEmpty()) continue;
 
-            points.add(new Tuple2<>(x1_out, y1_out));
-            points.add(new Tuple2<>(x2_out, y2_out));
-            points.add(new Tuple2<>(x, y3_out));
+                String[] coordValue = coord.trim().split(" ");
 
+                assert coordValue.length == 2;
+
+                final double x = Double.parseDouble(coordValue[0]);
+                final double y = Double.parseDouble(coordValue[1]);
+
+                originalPoints.add(new Tuple2<>(x, y));
+
+                final double x1_out = x - (eps * Math.sqrt(3)/2);
+                final double y1_out = y + (eps / 2);
+                final double x2_out = x + (eps * Math.sqrt(3)/2);
+                final double y2_out = y + (eps / 2);
+                final double y3_out = y - eps;
+
+                points.add(new Tuple2<>(x1_out, y1_out));
+                points.add(new Tuple2<>(x2_out, y2_out));
+                points.add(new Tuple2<>(x, y3_out));
+            }
+
+            Utils.writeToFile("tetrahedron.txt", coordsDefault);
             stage.close();
         });
     }
 
-    public List<Tuple2<Double, Double>> getTetrahedron() {
+    public Tuple2<List<Tuple2<Double, Double>>, List<Tuple2<Double, Double>>> getTetrahedron() {
         // Wait till the stage is closed
         stage.showAndWait();
-        return this.points;
+        return new Tuple2<>(this.originalPoints, this.points);
     }
 }
