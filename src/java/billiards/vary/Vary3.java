7ea9209af3b3f672548ac30b7709a4d2ef0b79bc
BlitzWrecker <wot.dragon0407@gmail.com>
2025-07-11 14:55:03 -0600

    replaced recursion with loop in Vary3.java

--- src/java/billiards/vary/Vary3.java
+++ src/java/billiards/vary/Vary3.java
@@ -1,75 +1,127 @@
 package billiards.vary;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.apache.commons.math3.util.FastMath;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.list.primitive.MutableIntList;
 import org.eclipse.collections.impl.list.mutable.FastList;
+import org.eclipse.collections.impl.list.mutable.primitive.BooleanArrayList;
+import org.eclipse.collections.impl.list.mutable.primitive.DoubleArrayList;
 import org.eclipse.collections.impl.list.mutable.primitive.IntArrayList;
 
 import billiards.codeseq.ClassifiedCodeSequence;
 import billiards.geometry.TriangleBilliard;
 import billiards.viewer.SideSum;
 
+import static billiards.vary.VaryCS.doneIteration;
+
 public final class Vary3 {
     private static final double OFFSET = 0.05;
 
-	private static void recurseFireAway(
-	    final int min, final int max, final double specMin, final double specMax, final double initPosition,
-	    final int depth, final SideSum sideSum, final TriangleBilliard billiard, final MutableIntList code,
-	    final MutableList<ClassifiedCodeSequence> codesFound) {
+	// Zhao Yu Li, Jul 11, 2025.
+	// Replaced recursion with a while loop.
+	private static void iterateFireAway(
+			final int min, final int max, final double initPosition,
+			final SideSum sideSum, final TriangleBilliard billiard, final MutableIntList code,
+			final MutableList<ClassifiedCodeSequence> codesFound) {
 
-		if (depth >= max) return;
-		if (Thread.currentThread().isInterrupted()) return;
+		IntArrayList sideSumArray = new IntArrayList();
+		DoubleArrayList specMinArray = new DoubleArrayList();
+		specMinArray.add(0);
 
-		if (depth > min) {
-			// here we check if we have reached a periodic path
+		DoubleArrayList specMaxArray = new DoubleArrayList();
+		specMaxArray.add(Math.PI);
 
-		    if (Math.abs(sideSum.sum()) < OFFSET && billiard.side == 2 && billiard.orient == 1) {
+		BooleanArrayList leftArray = new BooleanArrayList();
+		leftArray.add(false);
 
-		        final double perfectAngle = FastMath.atan2(billiard.vertexA.y, billiard.vertexA.x + initPosition);
+		BooleanArrayList rightArray = new BooleanArrayList();
+		rightArray.add(false);
 
-		        if (specMax > perfectAngle && perfectAngle > specMin) {
-		            final Optional<ClassifiedCodeSequence> codeSeq = Convert.convert(code);
-		            if (codeSeq.isPresent()) {
-		            	codesFound.add(codeSeq.get());
-		            }
-		        }
-		    }
-		}
+		ArrayList<TriangleBilliard> billiards = new ArrayList<>();
+		billiards.add(billiard);
 
-		final double specialAngle = billiard.getSpecialAngle();
+		int iterationDepth = 0;
 
-		if (specMax > specialAngle) {
-		    // go left
-		    final TriangleBilliard leftBilliard = billiard.getNext(true);
-		    final int leftSwap = 3 - billiard.side - leftBilliard.side;
+		while (true) {
+			if (iterationDepth >= max || Thread.currentThread().isInterrupted()) {
+				iterationDepth = doneIteration(code, sideSumArray, specMinArray, specMaxArray, leftArray, rightArray, billiards, iterationDepth, sideSum);
+				continue;
+			}
 
-		    sideSum.add(leftSwap);
-		    code.add(leftSwap);
+			boolean currentLeft = leftArray.getLast();
+			boolean currentRight = rightArray.getLast();
+			TriangleBilliard currentBilliard = billiards.get(billiards.size() - 1);
 
-		    recurseFireAway(min, max, Math.max(specialAngle, specMin), specMax, initPosition,
-		                    depth + 1, sideSum, leftBilliard, code, codesFound);
+			if (iterationDepth > min && !currentLeft && !currentRight) {
+				// here we check if we have reached a periodic path
 
-		    code.removeAtIndex(code.size() - 1);
-		    sideSum.sub(leftSwap);
-		}
+				if (Math.abs(sideSum.sum()) < OFFSET && currentBilliard.side == 2 && currentBilliard.orient == 1) {
+					final double perfectAngle = FastMath.atan2(currentBilliard.vertexA.y, currentBilliard.vertexA.x + initPosition);
 
-		if (specMin < specialAngle) {
-		    // go right
-		    final TriangleBilliard rightBilliard = billiard.getNext(false);
-		    final int rightSwap = 3 - billiard.side - rightBilliard.side;
+					if (specMaxArray.getLast() > perfectAngle && perfectAngle > specMinArray.getLast()) {
+						final Optional<ClassifiedCodeSequence> codeSeq = Convert.convert(code);
+                        codeSeq.ifPresent(codesFound::add);
+					}
+				}
+			}
 
-		    sideSum.sub(rightSwap);
-		    code.add(rightSwap);
+			final double specialAngle = currentBilliard.getSpecialAngle();
 
-		    recurseFireAway(min, max, specMin, Math.min(specialAngle, specMax), initPosition,
-		                    depth + 1, sideSum, rightBilliard, code, codesFound);
+			if (!currentLeft && !currentRight) {
+				if (specMaxArray.getLast() > specialAngle) {
+					// go left
+					final TriangleBilliard leftBilliard = currentBilliard.getNext(true);
+					final int leftSwap = 3 - currentBilliard.side - leftBilliard.side;
 
-		    code.removeAtIndex(code.size() - 1);
-		    sideSum.add(rightSwap);
-		}
+					sideSum.add(leftSwap);
+					sideSumArray.add(leftSwap);
+					code.add(leftSwap);
+
+					specMinArray.add(Math.max(specialAngle, specMinArray.getLast()));
+					specMaxArray.add(specMaxArray.getLast());
+					leftArray.add(false);
+					rightArray.add(false);
+					billiards.add(leftBilliard);
+
+					iterationDepth++;
+				} else {
+					currentLeft = true;
+					leftArray.set(leftArray.size() - 1, true);
+				}
+			}
+
+			if (currentLeft && !currentRight) {
+				if (specMinArray.getLast() < specialAngle) {
+					// go right
+					final TriangleBilliard rightBilliard = currentBilliard.getNext(false);
+					final int rightSwap = 3 - currentBilliard.side - rightBilliard.side;
+
+					sideSum.sub(rightSwap);
+					sideSumArray.add(rightSwap);
+					code.add(rightSwap);
+
+					specMinArray.add(specMinArray.getLast());
+					specMaxArray.add(Math.min(specialAngle, specMaxArray.getLast()));
+					leftArray.add(false);
+					rightArray.add(false);
+					billiards.add(rightBilliard);
+
+					iterationDepth++;
+				} else {
+					currentRight = true;
+					rightArray.set(rightArray.size() - 1, true);
+				}
+			}
+
+			if (currentLeft && currentRight) {
+				iterationDepth = doneIteration(code, sideSumArray, specMinArray, specMaxArray, leftArray, rightArray, billiards, iterationDepth, sideSum);
+			}
+
+			if (leftArray.isEmpty() && rightArray.isEmpty()) break;
+		}  // End of loop
 	}
 
 	public static MutableList<ClassifiedCodeSequence> fireAway(final int movesMin, final int movesMax,
@@ -80,7 +132,7 @@ public final class Vary3 {
         final SideSum sideSum = SideSum.create(xAngle, yAngle);
         final MutableIntList foundCode = new IntArrayList();
 
-        recurseFireAway(movesMin, movesMax, 0, Math.PI, pos, 0, sideSum, billiard, foundCode, codes);
+        iterateFireAway(movesMin, movesMax, pos, sideSum, billiard, foundCode, codes);
 		return codes;
 	}
 
