c111b61d5c65f5b364031e1a4e5e63a193438ba5
Marco <90290156+markmak233@users.noreply.github.com>
2025-08-21 10:05:59 -0600

    Update LinuxRun.sh
    remove software pipline

--- src/jarRunScript/LinuxRun.sh
+++ src/jarRunScript/LinuxRun.sh
@@ -1,5 +1,4 @@
 LD_LIBRARY_PATH=./backend/shared \
-java -Dprism.order=sw -Dprism.verbose=true \
-     -Djna.library.path=build/libs/backend/shared \
+java -Djna.library.path=build/libs/backend/shared \
      -Djava.library.path=build/libs/backend/shared \
      -jar ./billiard-viewer.jar
